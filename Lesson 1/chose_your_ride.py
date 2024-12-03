34dedb4f1d0759de12f713e1fae888a84f04937e
SanjuMu <sanjusri.munigala@gmail.com>
2024-12-03 17:25:09 -0500

    project

---
+++ Lesson 1/chose_your_ride.py
@@ -0,0 +1,13 @@
+print("Select your ride:")
+print("1. Bike")
+print("2. Car")
+
+
+if (choice == 1): 
+    print("What type of bike?")
+    print("1. Scooty\n")
+    print("2. Scooter\n")
+
+choice2 = int(input("Enter your 2nd choice: "))
+
+if choice 2==
---
+++ Lesson 1/project.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the age of the student: "))
+
+if (x >= 10):
+    if(x<=20):
+     print("The student can enrol with your class!!")
+
+else:
+    print("The student CAN'T enrol with your class!!")
