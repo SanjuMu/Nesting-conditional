e5a836d28c288b388eb435b1a69dd98f55904974
SanjuMu <sanjusri.munigala@gmail.com>
2024-12-08 20:33:12 -0500

    chose your ride

--- Lesson 1/chose_your_ride.py
+++ Lesson 1/chose_your_ride.py
@@ -1,1 +1,31 @@
 print("Select your ride: ")
+print("1.Bike")
+print("2.Car")
+
+choice = int (input("enter your choice:"))
+
+if( choice ==1):
+    print("what type of bike?")
+    print("1.scooty")
+    print("2.scooter")
+
+    if choice==1:
+        print("you have selected scooter")
+
+    else:
+        print("you have selected scooter")
+
+elif (choice == 2):
+    print("what type of car?")
+    print("1. Sedan")
+    print("2. XUV")
+    choice3 =int(input("enter your choice:"))
+
+    if choice3==1:
+        print("you have selected sedan")
+
+    else:
+        print("you have selected xuv")
+
+else:
+    print("Wrong Answer!! Check again")
